e9c01e968708c9ae01fbebb682ea69c7bb29ef4f
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2018-03-01 00:41:34 -0500

    Tweak deployment setup (see https://daveceddia.com/create-react-app-express-production/)

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ const db = new Client({
 // const db = require('./db.js')(config.databaseURL);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
