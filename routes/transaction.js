78e98ec7cb6dcac0891025de846fed70b92ad1dd
Ryan Lim <ryan.lim@yale.edu>
2018-05-01 14:26:03 -0400

    add transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -6,8 +6,8 @@ const transactionModels = require('../models/transaction.js');
 /* POST inserts new transaction */
 router.post('/add', (req, res) => {
   const db = req.app.get('db');
-  const { artId, email, phone, price} = req.body; 
-  consumerModels.addConsumer(db, artId, email, phone, price)
+  const { artId, email, phone, venmo, price} = req.body; 
+  transactionModels.addTransaction(db, artId, email, phone, venmo, price)
     .then(res.status(200).send())
     .catch(err => res.status(400).send({ error: err }));
 })
